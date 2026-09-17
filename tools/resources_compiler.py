10a0ae3d536a390aa3c67465b3b35bdf3fdfd2d6
BenjaminAM <benjamin.alan.mulkey@gmail.com>
2026-09-17 08:19:35 -0600

    Make resource compiler Python 3 compatible

--- tools/resources_compiler.py
+++ tools/resources_compiler.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.5
+#!/usr/bin/env python3
 #
 # Copyright 2012 Olivier Gillet.
 #
@@ -84,11 +84,11 @@ class ResourceEntry(object):
     if self._table.python_type == float:
       f.write('%(declaration)s = {\n' % locals())
       n_elements = len(self._value)
-      for i in xrange(0, n_elements, 4):
+      for i in range(0, n_elements, 4):
         f.write('  ');
         f.write(', '.join(
             '% 16.9e' % self._value[j] \
-            for j in xrange(i, min(n_elements, i + 4))))
+            for j in range(i, min(n_elements, i + 4))))
         f.write(',\n');
       f.write('};\n')
     elif self._table.python_type == str:
@@ -97,12 +97,12 @@ class ResourceEntry(object):
     else:
       f.write('%(declaration)s = {\n' % locals())
       n_elements = len(self._value)
-      for i in xrange(0, n_elements, 4):
+      for i in range(0, n_elements, 4):
         f.write('  ');
         f.write(', '.join(
             '%6d' % self._value[j] if self._value[j] < 1 << 31 else \
                 '%6dUL' % self._value[j] \
-            for j in xrange(i, min(n_elements, i + 4))))
+            for j in range(i, min(n_elements, i + 4))))
         f.write(',\n');
       f.write('};\n')
     
@@ -143,19 +143,19 @@ class ResourceTable(object):
     in_chr = ''.join(map(chr, range(256)))
     out_chr = [ord('_')] * 256
     # Tolerated characters.
-    for i in string.uppercase + string.lowercase + string.digits:
+    for i in string.ascii_uppercase + string.ascii_lowercase + string.digits:
       out_chr[ord(i)] = ord(i.lower())
-
     # Rewritten characters.
     in_rewritten = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09~*+=><^"|'
     out_rewritten = '0123456789TPSeglxpv'
     for rewrite in zip(in_rewritten, out_rewritten):
       out_chr[ord(rewrite[0])] = ord(rewrite[1])
+    table = str.maketrans(
+        in_chr,
+        ''.join(map(chr, out_chr)),
+        '\t\n\r-:()[]\"\',;')
+    self._MakeIdentifier = lambda s: s.translate(table)
 
-    table = string.maketrans(in_chr, ''.join(map(chr, out_chr)))
-    bad_chars = '\t\n\r-:()[]"\',;'
-    self._MakeIdentifier = lambda s:s.translate(table, bad_chars)
-  
   def DeclareEntries(self, f):
     if self.python_type != str:
       for entry in self.entries:
@@ -227,7 +227,7 @@ class ResourceLibrary(object):
   
   def GenerateHeader(self):
     root = self._root
-    f = file(os.path.join(root.target, 'resources.h'), 'wb')
+    f = open(os.path.join(root.target, 'resources.h'), 'w')
     # Write header and header guard
     header_guard = root.target.replace(os.path.sep, '_').upper()
     header_guard = '%s_RESOURCES_H_' % header_guard
@@ -248,7 +248,7 @@ class ResourceLibrary(object):
   def GenerateCc(self):
     root = self._root
     file_name = os.path.join(self._root.target, 'resources.cc')
-    f = file(file_name, 'wb')
+    f = open(file_name, 'w')
     f.write(self._root.header + '\n\n')
     f.write('#include "%s"\n' % file_name.replace('.cc', '.h'))
     self._OpenNamespace(f)
@@ -272,7 +272,7 @@ def Compile(path):
 
 
 def main(argv):
-  for i in xrange(1, len(argv)):
+  for i in range(1, len(argv)):
     Compile(argv[i])
 
 
